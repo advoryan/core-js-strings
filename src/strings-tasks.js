f821a43be13ee773c47b6f311555a16e80792b7c
advoryan <a.dvoryan@gmail.com>
2026-09-23 00:37:47 +0200

    feat: remove Occurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -168,8 +168,12 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const idx = str.lastIndexOf(value);
+
+  if (idx === -1) return str;
+
+  return str.slice(0, idx) + str.slice(idx + value.length);
 }
 
 /**

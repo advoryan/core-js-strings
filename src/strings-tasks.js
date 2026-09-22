c817ee9befc0c32fb7970409468bd95b75e18555
advoryan <a.dvoryan@gmail.com>
2026-09-23 01:16:39 +0200

    feat: extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -410,8 +410,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.replace(/^Hello, (.*)!$/, '$1');
 }
 
 /**
